2052b6c4547c20498a4f818b3e2aae3d2f2e0c21
Victor Mejia <victormejia@users.noreply.github.com>
2019-10-03 15:00:40 -0700

    02-06: auth guards (#7)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToProfile = () => 
+  map(user => user ? ['profile', (user as any).uid]: true);
+
+const onlyAllowSelf = next => 
+  map(
+    user => (!!user && next.params.id == (user as any).uid) || ['']
+  );
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToProfile }
   },
   {
     path: 'profile/:id',
-    component: ProfileComponent
+    component: ProfileComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: onlyAllowSelf }
   }
 ];
 
