04c1f449d1e8ed97de9d8d832bc45743db9449b9
Victor Mejia <victor.a.mejia@gmail.com>
2019-10-03 14:29:51 -0700

    02-05: user login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loading = false;
+  action: 'login' | 'signup' = 'login';
+  error: string;
 
   constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
@@ -18,18 +20,35 @@ export class LoginComponent implements OnInit {
 
   async onSubmit(form: NgForm) {
     this.loading = true;
+    this.error = null;
+
     const { email, password, firstName, lastName } = form.value;
 
+    let resp;
     try {
-      const resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
-      await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
-      form.reset();
+      if (this.isSignUp) {
+        resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+        await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
+        form.reset();
+      } else {
+        resp = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+      }
+
       const uid = resp.user.uid;
       this.router.navigate([`/profile/${uid}`]);
     } catch (error) {
       console.log(error.message);
+      this.error = error.message;
     }
     this.loading = false;
+  }
+
+  get isLogin() {
+    return this.action === 'login';
+  }
+
+  get isSignUp() {
+    return this.action === 'signup';
   }
 
 }
