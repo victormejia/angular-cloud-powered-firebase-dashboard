732389ba6c30fc72c75e6897b58949f2227a2225
Victor Mejia <victor.a.mejia@gmail.com>
2019-10-03 13:08:45 -0700

    02-02: create user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loading = false;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  async onSubmit(form: NgForm) {
+    this.loading = true;
+    const { email, password, firstName, lastName } = form.value;
+
+    try {
+      const resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+      await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
+      form.reset();
+    } catch (error) {
+      console.log(error.message);
+    }
+    this.loading = false;
   }
 
 }
