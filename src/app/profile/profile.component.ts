7affe77932789637eb1e066b0f1db6e0467db2b7
Victor Mejia <victormejia@users.noreply.github.com>
2019-10-03 16:31:42 -0700

    03-05: display new profile document (#10)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { UserProfile } from '../core/user-profile.model';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +10,14 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  private itemDoc: AngularFirestoreDocument<UserProfile>;
+  item: Observable<UserProfile>;
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.itemDoc = this.afs.doc<UserProfile>(`users/${this.afAuth.auth.currentUser.uid}`);
+    this.item = this.itemDoc.valueChanges();
   }
 
 }
