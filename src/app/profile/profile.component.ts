3200e2471240100711d219ff7c6dbe59bc6748b3
Victor Mejia <victor.a.mejia@gmail.com>
2019-10-04 09:39:46 -0700

    03-06: update profile data

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { UserProfile } from '../core/user-profile.model';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,12 +15,61 @@ import { UserProfile } from '../core/user-profile.model';
 export class ProfileComponent implements OnInit {
   private itemDoc: AngularFirestoreDocument<UserProfile>;
   item: Observable<UserProfile>;
+  uid: string;
 
-  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) { }
+  loading = false;
+  error: string;
+
+  constructor(
+    public afAuth: AngularFireAuth, 
+    public afs: AngularFirestore, 
+    private route: ActivatedRoute,
+    private auth: AuthService
+  ) {
+    this.uid = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
-    this.itemDoc = this.afs.doc<UserProfile>(`users/${this.afAuth.auth.currentUser.uid}`);
+    this.itemDoc = this.afs.doc<UserProfile>(`users/${this.uid}`);
     this.item = this.itemDoc.valueChanges();
+  }
+
+  async onSubmit(ngForm: NgForm) {
+    // this.loading = true;
+
+    const { 
+      email,
+      name,
+      address,
+      city,
+      state,
+      zip,
+      ip,
+      phone,
+      specialty
+    } = ngForm.form.getRawValue();
+
+    const userProfile: UserProfile = {
+      uid: this.uid,
+      email,
+      name,
+      address,
+      city,
+      state,
+      zip,
+      ip,
+      phone,
+      specialty
+    };
+
+    try {
+      await this.auth.updateUserDocument(userProfile);
+    } catch(error) {
+      console.log(error.message);
+      this.error = error.message;
+    }
+
+    this.loading = false;
   }
 
 }
