abeca9f0cf8954e562284cb05de242e4539fdf26
Victor Mejia <victor.a.mejia@gmail.com>
2019-10-04 15:57:15 -0700

    06-02: cloud functions email setup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// for sending email:
+// 1. enable access to less secure apps:
+//    https://www.google.com/settings/security/lesssecureapps
+// 2. Display Unlock Captcha:
+//    https://accounts.google.com/DisplayUnlockCaptcha
+// For a gmail account with 2-step verification enabld, you will need to use
+// an app password:
+//    https://support.google.com/accounts/answer/185833
