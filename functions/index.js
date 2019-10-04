d7bd07f34be2fb10a1dc4765966b7820bb7a0e53
Victor Mejia <victor.a.mejia@gmail.com>
2019-10-04 15:37:39 -0700

    reset default function code

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,8 @@
 const functions = require('firebase-functions');
 
-// for sending email:
-// 1. enable access to less secure apps:
-//    https://www.google.com/settings/security/lesssecureapps
-// 2. Display Unlock Captcha:
-//    https://accounts.google.com/DisplayUnlockCaptcha
-// For a gmail account with 2-step verification enabld, you will need to use
-// an app password:
-//    https://support.google.com/accounts/answer/185833
-
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
